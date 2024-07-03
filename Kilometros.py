183eab53cd7a0e8cfc54e9538b314c8cae4e40be
intimatute06 <168133152+intimatute06@users.noreply.github.com>
2024-07-03 12:19:18 -0500

    Add files via upload

---
+++ Kilometros.py
@@ -0,0 +1,6 @@
+kilometros = 12.25
+miles = 7.38
+miles_a_kilometros = miles * 1.61
+kilometros_a_miles = kilometros / 1.61
+print(miles,"millas son",round(miles_a_kilometros,2),"kilometros")
+print(kilometros,"kilometros son",round(kilometros_a_miles,2),"millas")
