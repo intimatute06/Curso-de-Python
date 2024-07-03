47fd6a1f83df1d450681c41ccad4a3aa0b2f7db8
intimatute06 <168133152+intimatute06@users.noreply.github.com>
2024-07-03 12:13:12 -0500

    Add files via upload

---
+++ Manzanas.py
@@ -0,0 +1,6 @@
+juan = 3
+maria = 5
+adan = 6
+print("Juan tiene", juan,"manzanas, Maria tiene", maria,"manzanas y Adan tiene", adan,)
+total_manzanas = juan + maria + adan
+print("\nLa cantidad total de manzanas es", total_manzanas)
